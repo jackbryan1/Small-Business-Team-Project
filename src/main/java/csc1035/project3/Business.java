2cbb1c72633b3507e6c3fec6a7c53ca5295aa914
b9032016 <b.summers1@newcastle.ac.uk>
2020-02-25 13:04:48 +0000

    Created an interface.

---
+++ src/main/java/csc1035/project3/Business.java
@@ -0,0 +1,14 @@
+package csc1035.project3;
+
+public interface Business {
+
+    int availableStock(Item item);
+    /**
+     * Check the available stock of an item.
+     *
+     * @param : Item the item to check stock.
+     * @return : Returns the stock available.
+     */
+
+    
+}
