3f00d062b196dea32e1951475d8e9289eed71b72
b9005046 <b9005046@newcastle.ac.uk>
2020-02-25 13:41:54 +0000

    Updated Database name and password in hibernate config. Begun Database class.

---
+++ src/main/java/csc1035/project3/Database.java
@@ -0,0 +1,14 @@
+package csc1035.project3;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Database {
+    // Load database.
+
+}
