bb2b037a177dc29efb1f945d337a117a7cd1dc13
b9038224 <b9038224@newcastle.ac.uk>
2020-02-26 11:50:47 +0000

    Created TrickyTrinkets class which implements Business - methods do not currently do anything

---
+++ src/main/java/csc1035/project3/TrickyTrinkets.java
@@ -0,0 +1,11 @@
+package csc1035.project3;
+
+public class TrickyTrinkets implements Business {
+
+    public int availableStock(Item item){return 0;}
+
+    public String printReceipt(Item item, Customer customer){return null;}
+
+    public void updateStock(Item item){}
+
+}
