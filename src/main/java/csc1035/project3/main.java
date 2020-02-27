ce9c0c2723a218538909f853fc2acd27d38b3ff6
b9035223 <b9035223@ncl.ac.uk>
2020-02-27 14:30:24 +0000

    Added basic commands to CLI.

--- src/main/java/csc1035/project3/main.java
+++ src/main/java/csc1035/project3/main.java
@@ -12,8 +12,13 @@ public class main {
         while(true){
             System.out.println("Next Command");
             System.out.println("[0]:End \n" +
-                            "[1]:Command 1 \n" +
-                            "[2]:Command 2 \n");
+                            "[1]:Print Receipt \n" +
+                            "[2]:Add new item \n" +
+                            "[3]:Check stock \n" +
+                            "[4]:Update stock \n" +
+                            "[5]:Delete item \n" +
+                            "[6]:Transaction \n"
+                    );
             s = scanner.nextLine();
             switch(s) {
                 default:
@@ -23,11 +28,22 @@ public class main {
                     System.out.println("Program closed");
                     return;
                 case ("1"):
-                    System.out.println("Runs command 1");
+                    System.out.println("Prints receipt");
                     break;
                 case ("2"):
-                    System.out.println("Runs command 2");
+                    System.out.println("Add new item");
                     break;
+                case ("3"):
+                    System.out.println("Check stock");
+                    break;
+                case ("4"):
+                    System.out.println("Update stock");
+                    break;
+                case ("5"):
+                    System.out.println("Delete item");
+                    break;
+                case ("6"):
+                    System.out.println("Starts transaction");
             }
         }
     }
