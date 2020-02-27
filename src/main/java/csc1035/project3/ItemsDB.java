e844ede7c9394cf8ca36561848bac387bc866be5
b9005046 <b9005046@newcastle.ac.uk>
2020-02-27 14:19:42 +0000

    Added create method to ItemDB class that allows for multiple items at once to be added to the items database.

--- src/main/java/csc1035/project3/ItemsDB.java
+++ src/main/java/csc1035/project3/ItemsDB.java
@@ -1,8 +1,16 @@
 package csc1035.project3;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 public class ItemsDB {
-    public static void create(Item item) {
+    public static void create(Item[] itemArray) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
+        for (Item item : itemArray) {
+            session.save(item);
+        }
+        session.getTransaction().commit();
+        session.close();
     }
 }
