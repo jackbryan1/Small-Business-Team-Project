18f02e6e6972b6364fae1a06d2124fe94838a329
b9005046 <b9005046@newcastle.ac.uk>
2020-02-27 14:52:39 +0000

    Added read method to ItemDB class that fetches all items in database into a list. Removed declaration of id in Item object since it is auto generated.

--- src/main/java/csc1035/project3/ItemsDB.java
+++ src/main/java/csc1035/project3/ItemsDB.java
@@ -1,16 +1,40 @@
 package csc1035.project3;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsDB {
-    public static void create(Item[] itemArray) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        for (Item item : itemArray) {
-            session.save(item);
+
+    Session session;
+
+    // Adds item objects to the item database from an arraylist.
+    public void create(ArrayList<Item> itemArray) {
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            for (Item item : itemArray) {
+                session.save(item);
+            }
+            session.getTransaction().commit();
+            session.close();
+        } catch (HibernateException e) {
+            if (session != null) session.getTransaction().rollback();
+                e.printStackTrace();
+            }
         }
-        session.getTransaction().commit();
-        session.close();
-    }
+
+    public void read() {
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            List Items = session.createQuery("FROM Item").list();
+            session.close();
+        } catch (HibernateException e) {
+        if (session != null) session.getTransaction().rollback();
+        e.printStackTrace();
+    } 
+
 }
