9a8e3269e7585cca2a8c5fb95e72a55481fd1966
b9005046 <b9005046@newcastle.ac.uk>
2020-02-28 14:26:19 +0000

    Switched from auto to table generation for CustomerID.

--- src/main/java/csc1035/project3/Customers.java
+++ src/main/java/csc1035/project3/Customers.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "Customers")
 public class Customers {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "cid", updatable = false, nullable = false)
     private int cid;
     @Column(name = "surname")
