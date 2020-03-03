e6b1ea5caeadae6c120b5a965e70b72898203929
b9032016 <b.summers1@newcastle.ac.uk>
2020-03-03 12:54:48 +0000

    Updated Transaction class to contain both getters and setters.

--- src/main/java/csc1035/project3/Transaction.java
+++ src/main/java/csc1035/project3/Transaction.java
@@ -2,4 +2,12 @@ package csc1035.project3;
 
 public class Transaction {
     private int tid;
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
 }
