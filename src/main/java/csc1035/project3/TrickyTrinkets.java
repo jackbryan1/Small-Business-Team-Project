2f1d274cba6c7ad6a93b7639d915dbc4bf543e5c
b9032016 <b.summers1@newcastle.ac.uk>
2020-03-03 13:30:43 +0000

    Started implementing the availableStock method from the interface. Currently the query is broken however, needs fixing.

--- src/main/java/csc1035/project3/TrickyTrinkets.java
+++ src/main/java/csc1035/project3/TrickyTrinkets.java
@@ -1,8 +1,28 @@
 package csc1035.project3;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import javax.management.Query;
+import java.util.List;
+
+
 public class TrickyTrinkets implements Business {
 
-    public int availableStock(Items item){return 0;}
+    static Session session;
+
+    public int availableStock(Items item){
+        try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            String available = session.createQuery("SELECT stock FROM Items i WHERE i.name = item").toString();
+            session.close();
+            return Integer.parseInt(available);
+        } catch(HibernateException e){
+            if(session != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
     public String printReceipt(Items item, Customers customer, Transaction transaction){return null;}
 
