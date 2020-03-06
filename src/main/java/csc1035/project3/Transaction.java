8630af80418ae169a9cc2e2fb5a01336995a5d61
b9015402 <b9015402@newcastle.ac.uk>
2020-03-06 14:10:37 +0000

    Added price calculation to transaction

--- src/main/java/csc1035/project3/Transaction.java
+++ src/main/java/csc1035/project3/Transaction.java
@@ -5,12 +5,13 @@ import java.util.HashMap;
 public class Transaction {
 
 
-    public static void transaction(HashMap<Items, Integer> items){
-
+    public static float transaction(HashMap<Items, Integer> items){
+        float price = 0;
         for( Items item: items.keySet()){
+            price += items.get(item) * item.getSellPrice();
             item.setStock(item.getStock() - items.get(item));
             ItemsDB.update(item);
         }
-
+        return price;
     }
 }
