8c80b974eddac871c5c55fe39dab9c297e09e1ff
b9005046 <b9005046@newcastle.ac.uk>
2020-03-11 11:29:35 +0000

    Added annotations to the interface methods. Added a test method in the tests package for the items database.

--- src/main/java/csc1035/project3/Business.java
+++ src/main/java/csc1035/project3/Business.java
@@ -3,13 +3,37 @@ package csc1035.project3;
 import java.util.ArrayList;
 
 public interface Business {
+    /**
+     * Takes an arrayList of Items class objects and adds them to the Items database.
+     * @param Items An arrayList of Items to be created in the database.
+     */
     void addItem(ArrayList<Items> Items);
 
+    /**
+     * Checks stock of an item in the Items database.
+     * @param id The id of the item to check the stock of.
+     * @param options The available id's to choose from as specified by the main method.
+     * @return Returns the stock of the item with matching id.
+     */
     int checkStock(Integer id, ArrayList options);
 
+    /**
+     * Updates the stock of an item in the items database.
+     * @param id The id of the item to update the stock of.
+     * @param stock The new stock to set.
+     * @param options The available id's to choose from as specified by the main method.
+     */
     void updateStock(Integer id, Integer stock, ArrayList options);
 
+    /**
+     * Deletes an item from the items database.
+     * @param id The id of the item to delete.
+     * @param options The available id's to choose from as specified by the main method.
+     */
     void deleteItem(Integer id, ArrayList options);
 
+    /**
+     * Allows for manipulation of the customer database.
+     */
     void customerDB();
 }
---
+++ src/main/java/project3Tests/ItemsDBTest.java
@@ -0,0 +1,27 @@
+package project3Tests;
+
+import csc1035.project3.Items;
+import csc1035.project3.ItemsDB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsDBTest {
+    // This class is to test CRUD implementation works.
+    // Use test by uncommenting the lines and altering as necessary.
+    public static void main(String[] args) {
+        //Items item = new Items("test", "test", true, 13.00, 14, 14.00);
+        //ArrayList<Items> test = new ArrayList<>();
+        //Items item = idSearch(10);
+        //test.add(item);
+        //test.add(item);
+        //Transaction.transaction(test);
+        //test.add(item);
+        //create(test);
+        //Items test = idSearch(10);
+        //test.setName("Large Animal Sculpture");
+        //update(test);
+        //readSearch("");
+        //delete(test);
+    }
+}
